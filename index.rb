4fc0e098949ad6f12bfe5aab851568004fa93e16
errea <erreakezy@gmail.com>
2021-03-18 23:11:20 +0100

    add test case for my_each_with_index method in index.rb

--- index.rb
+++ index.rb
@@ -3,3 +3,14 @@ require_relative 'enumerable'
 # my_each test case
 a = %w[a b c]
 a.my_each { |x| print x, ' -- ' }
+
+#my_each_with_index test case
+hash = Hash.new
+%w(cat dog wombat).each_with_index { |item, index|
+  hash[item] = index
+}
+p hash   
+
+%w(cat dog wombat).my_each_with_index { |item, index|
+  p  index.to_s + "_" + item
+  }
