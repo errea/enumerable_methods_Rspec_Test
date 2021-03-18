c82bec936cc59ae7c6bf9e5f6788fa183fedbd73
errea <erreakezy@gmail.com>
2021-03-18 23:06:51 +0100

    add syntax for enumerable.rb file

--- enumerable.rb
+++ enumerable.rb
@@ -1,8 +1,18 @@
 module Enumerable
+    #my_each method
   def my_each
     return to_enum(:my_each) unless block_given?
 
     to_a.length.times { |i| yield to_a[i] }
     self
   end
-end
+
+  #each_with_index
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+
+    to_a.length.times { |i| yield to_a[i], i }
+    self
+  end  
+  end
+
