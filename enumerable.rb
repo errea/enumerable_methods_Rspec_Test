e9b2fb539ab8ee7e33c83906229b178b75e58e29
John Arboleda <JohnJack0126@gmail.com>
2021-03-19 10:10:38 -0500

    all? implementation

--- enumerable.rb
+++ enumerable.rb
@@ -1,5 +1,7 @@
 module Enumerable
+
   # my_each method
+
   def my_each
     return to_enum(:my_each) unless block_given?
 
@@ -8,6 +10,7 @@ module Enumerable
   end
 
   # each_with_index
+
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
 
@@ -15,10 +18,42 @@ module Enumerable
     self
   end
 
+  # my_select
+
   def my_select
     return to_enum(:my_select) unless block_given?
-    
-    to_a.length.times { |i| yield(to_a[i]).positive? }
-    self
+
+    arr = []
+    to_a.length.times { |i| arr.push(to_a[i]) if yield to_a[i] }
+    arr
   end
+
+  # my_all?
+
+  def my_all?(arg = true)
+
+    bool = true
+    if block_given?
+      to_a.length.times { |i| bool = bool && false unless yield to_a[i] }
+    elsif arg == true || arg == false
+      to_a.length.times { |i| bool = bool && false unless to_a[i] }
+    elsif not arg.instance_of?(Class)
+      to_a.length.times { |i| bool = bool && false unless to_a[i] == arg }
+    else
+      to_a.length.times { |i| bool = bool && false unless to_a[i].is_a?(arg) }
+    end
+    bool
+  end
+
+  # my_any?
+
+  def my_any?
+
+    bool = true
+    to_a.length.times { |i| bool && false unless yield to_a[i] }
+    bool
+  end
+
+  # my_none?
+
 end
--- index.rb
+++ index.rb
@@ -16,6 +16,24 @@ p hash
 end
 
 # my_select test case
-
+p (1..10).my_select { |i|  i % 3 == 0 }   #=> [3, 6, 9]
 p [1,2,3,4,5].my_select { |num|  num.even?  }   #=> [2, 4]
 p [1,2,3,4,5].my_select { |num|  num > 3  }   #=> [4, 5]
+p [:foo, :bar].my_select { |x| x == :foo }
+
+# all?
+p %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
+p %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
+p %w[ant bear cat].my_all?(/t/)                        #=> false
+p [1, 2i, 3.14].my_all?(Numeric)                       #=> true
+p [nil, true, 99].my_all?                              #=> false
+p [].my_all?                                           #=> true
+
+# any?
+%w[ant bear cat].any? { |word| word.length >= 3 } #=> true
+%w[ant bear cat].any? { |word| word.length >= 4 } #=> true
+%w[ant bear cat].any?(/d/)                        #=> false
+[nil, true, 99].any?(Integer)                     #=> true
+[nil, true, 99].any?                              #=> true
+[].any?                                           #=> false
+
