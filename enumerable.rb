59bf094ee95ced59b9e572d455b61fd4c2a02aa1
errea <erreakezy@gmail.com>
2021-03-19 16:21:41 +0100

    add the all, any and none methods to test branch

--- enumerable.rb
+++ enumerable.rb
@@ -7,7 +7,7 @@ module Enumerable
     self
   end
 
-  # each_with_index
+  # each_with_index method
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
 
@@ -15,10 +15,60 @@ module Enumerable
     self
   end
 
-  def my_select
-    return to_enum(:my_select) unless block_given?
+  #select method
+ # def my_select
+   # return to_enum(:my_select) unless block_given?
     
-    to_a.length.times { |i| yield(to_a[i]).positive? }
-    self
+    #to_a.my_even { |i| yield(to_a[i]).positive? }
+    #self
+  #end
+#end
+
+#All method
+def my_all?(array = true)
+  statement = true
+  my_each do |index|
+    if block_given?
+      statement = false unless yield(index)
+    elsif array.nil?
+      statement = false unless index
+    else
+      statement = false unless array === index
+    end
   end
+  statement
+end
+
+#Any method
+def my_all?(array = nil)
+  statement = false
+  my_each do |index|
+    if block_given?
+      statement = true if yield(index)
+    elsif array.nil?
+      statement = true if index
+    elsif array  === index
+      statement = true
+    end
+  end
+  statement
+end
+
+
+#None Method
+def my_none?(array = nil)
+  statement = true
+  my_each do |index|
+    if block_given?
+      statement = false if yield index
+    elsif array.nil?
+      statement = false if index
+    elsif array === index
+      my_each { |_index| statement = false }
+    end
+  end
+  statement
+end
+
+
 end
--- index.rb
+++ index.rb
@@ -16,6 +16,24 @@ p hash
 end
 
 # my_select test case
+#p [1,2,3,4,5].my_select { |num|  num.even?  }   #=> [2, 4]
+#p [1,2,3,4,5].my_select { |num|  num > 3  }   #=> [4, 5]
 
-p [1,2,3,4,5].my_select { |num|  num.even?  }   #=> [2, 4]
-p [1,2,3,4,5].my_select { |num|  num > 3  }   #=> [4, 5]
+#my_all test case
+p %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
+p %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
+p %w[ant bear cat].my_all?(/t/)                        #=> false
+p [1, 2i, 3.14].my_all?(Numeric)                       #=> true
+p [nil, true, 99].my_all?                              #=> false
+p [].my_all?                                           #=> true                         
+
+
+
+#my_any test case
+=begin %w[ant bear cat].my_any? { |word| word.length >= 3 } #=> true
+%w[ant bear cat].my_any? { |word| word.length >= 4 } #=> true
+%w[ant bear cat].my_any?(/d/)                        #=> false
+[nil, true, 99].my_any?(Integer)                     #=> true
+[nil, true, 99].my_any?                              #=> true
+[].my_any?                                           #=> false
+ =end
